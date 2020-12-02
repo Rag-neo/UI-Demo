de92e879493563f7cf3785111de11995be211211
Kiran <kiran.r@neoito.com>
2020-12-02 10:10:42 +0530

    fixes for responsive

--- charts-demo/tailwind.config.js
+++ charts-demo/tailwind.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.ts', './projects/**/*.html', './projects/**/*.ts'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
-    extend: {
-      colors:{
- 
-      },
-      screens:{
-        'mobile':{'max':'640px'}
-      }
+    screens: {
+      sm: '639px',
+      md: '767px',
+      lg: '1025px',
+      xl: '1279px',
     },
+    container: {
+      screens: {
+         sm: "100%",
+         md: "100%",
+         lg: "1024px",
+         xl: "1024px"
+      }
+    }
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
